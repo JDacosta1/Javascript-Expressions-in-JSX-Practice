7c6748b259f2c596f8ffcc006c39fef4fdde939c
JDacosta <56207099+NeoReformed@users.noreply.github.com>
2023-08-09 11:23:23 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+import React from "react";
+import ReactDOM from "react-dom";
+
+const yourName = "Jardine Dacosta";
+const date = document.write(new Date().getFullYear());
+ReactDOM.render(
+  <div>
+    <p>Created by {yourName}.</p>
+    <p>Copyright {date}.</p>
+  </div>,
+  document.getElementById("root")
+);
